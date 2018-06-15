4d7e21fba60183fe3ea6224c1f58f058914aaa5c
Cj <cj@Cjs-MacBook-Pro.local>
2018-06-15 12:08:11 -0700

    add blog

--- blogs/models.py
+++ blogs/models.py
@@ -5,6 +5,7 @@ class Blog(models.Model):
 
     title   = models.CharField(max_length=500)
     content = models.TextField(max_length=10000)
+    image   = models.CharField(max_length=100)
 
 
     def __str__(self):
